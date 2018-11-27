1edeae72bacaf8f5e65276c63ae3515a6184233a
gadael <gsedubun@gmail.com>
2018-11-27 17:43:57 +0700

    initial
    Signed-off-by: gadael <gsedubun@gmail.com>

---
+++ kafka_producer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+using System.Collections.Generic;
+
+namespace kafka_producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello kafka producer...!");
+
+	    var config = new Dictionary<string, object>{
+	    	{ "bootstrap.servers", "localhost:9092" }
+	    
+	    };
+
+	    using(var producer = new Producer<Null, string>(config, null, new StringSerializer(Encoding.UTF8)))
+	    {
+	    	string text=null;
+		while(text!="exit"){
+			text = Console.ReadLine();
+			producer.ProduceAsync("hello-topic", null, text);
+		}
+
+		producer.Flush(100);
+
+	    }
+
+        }
+    }
+}
